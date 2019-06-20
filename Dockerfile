c753f14f241b39f1541754529a31c3805657cfbf
Ryan McGuire <ryan@enigmacurry.com>
2019-06-20 09:41:34 -0400

    Logging

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM tiangolo/uvicorn-gunicorn-fastapi:python3.7-alpine3.8
-
+COPY ./requirements.txt /tmp/requirements.txt
+RUN pip install -r /tmp/requirements.txt && rm /tmp/requirements.txt
 COPY ./app /app
--- app/main.py
+++ app/main.py
@@ -2,23 +2,28 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 from starlette.status import HTTP_400_BAD_REQUEST
+from typing import Dict
+import requests
 import logging
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("fastapi").setLevel(logging.DEBUG)
+
+class SlackEvent(BaseModel):
+    type: str
+    event_ts: str
 
 class SlackEventType(BaseModel):
     type: str
     token: str = None
     team_id: str = None
     api_app_id: str = None
-    event: SlackEvent = None
+    event: Dict[str, str] = None
 
 class SlackUrlVerificationEvent(SlackEventType):
     challenge: str
 
-class SlackEvent(BaseModel):
-    type: str
-    event_ts: str
-
-class SlackAppMentionEvent(SlackEvent)
+class SlackAppMentionEvent(SlackEvent):
     user: str
     text: str
     ts: str
@@ -27,14 +32,14 @@ class SlackAppMentionEvent(SlackEvent)
 app = FastAPI()
 
 @app.post("/events/")
-def receive_event(event_wrapper: SlackEventType):
+def receive_event(event_wrapper: Dict [str, str]):
     """
     Respond to Slack events
     """
     # TODO : verify slack signature
-
+    logging.info("Receive event type: {et}".format(et=event_wrapper))
     if event_wrapper.type == "url_verification":
-        event = SlackUrlVerificationEvent(event_wrapper)
+        event = SlackUrlVerificationEvent.parse_obj(event_wrapper)
         # Respond to initial URL verification:
         # See: https://api.slack.com/events-api#events_api_request_urls
         return {"challenge": event.challenge}
@@ -47,8 +52,21 @@ def receive_event(event_wrapper: SlackEventType):
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST,
                             detail="Unhandled event type")
 
-def handle_event_callback(event: SlackEvent):
+def handle_event_callback(event: Dict[str, str]):
     if event.type == "app_mention":
-        event = SlackAppMentionEvent(event)
+        logging.info(event)
+        event = SlackAppMentionEvent.parse_obj(event)
         logging.info("Received app mention: {e}".format(e=event))
+        send_message(event.channel, "Hello @{user}".format(user=event.user))
         return
+    else:
+        logging.error("Cannot handle event: {e}".format(e=event))
+
+def send_message(channel, message):
+    try:
+        webhook_url = os.environ("SLACK_WEBHOOK_{channel}".format(channel=channel))
+    except KeyError:
+        logging.error("No known webhook for channel: {channel} - "
+                      "must provide SLACK_CHANNEL_{channel} environment variable.".format(channel=channel))
+        raise
+    requests.post(webhook_url, json={"text": message})
