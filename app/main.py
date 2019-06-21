769fd6a4b466aacaf02995fad74b5f899d58c915
Ryan McGuire <ryan@enigmacurry.com>
2019-06-21 15:06:47 -0400

    Implements Slack signature verfication but breaks everything else
    fastapi middleware that accesses the body, consumes it. Making everything that
    comes after the middleware see a zero-length body. :(

--- app/main.py
+++ app/main.py
@@ -1,27 +1,57 @@
-from fastapi import FastAPI
-from fastapi import FastAPI, HTTPException
-from starlette.status import HTTP_400_BAD_REQUEST
+import os
+import time
+from fastapi import FastAPI, Header, Body, HTTPException
+from starlette.requests import Request
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED
 from typing import Dict
 import logging
+import json
+import traceback
 
-from slack import send_message, slack_users_info
 from slack_model import SlackEventType, SlackUrlVerificationEvent, SlackAppMentionEvent
+import slack
 
 
-logging.basicConfig(level=logging.INFO)
-
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger("main")
+logger.setLevel(logging.DEBUG)
 app = FastAPI()
 
-@app.post("/events/")
-def receive_event(event_wrapper: dict):
-    """
-    Respond to Slack events
-    """
-    event_wrapper = SlackEventType(**event_wrapper)
-    logging.debug("Receive event type: {et}".format(et=event_wrapper))
 
-    # TODO : verify slack signature
+### Config
+
+class ConfigurationException(Exception):
+    pass
+
+config = {}
+required_config = ["SLACK_SIGNING_SECRET", "SLACK_TOKEN"]
+try:
+    for var in required_config:
+        config[var] = os.environ[var]
+    for name, webhook in [(k,v) for (k,v) in os.environ.items() \
+                          if k.startswith("SLACK_WEBHOOK_")]:
+        config[name] = webhook
+        logger.info("Found slack webhook config: {webhook}".format(
+            webhook=webhook))
+except KeyError:
+    raise ConfigurationException("Missing environment variable: {var}".format(var=var))
+
+### Handlers
+
+@app.post("/events/")
+def receive_event(body = Body(...)):
+    """
+    Respond to Slack events.
+
+    Don't do any pre-validation of the body, just get the raw string.
+    This is necessary to perform signature verification via HMAC SHA256.
+    """
+
+    event_wrapper = SlackEventType(**body)
+    logger.debug("Receive event type: {et}".format(et=event_wrapper))
+
     if event_wrapper.type == "url_verification":
+        # One time event by slack to verify our endpoint
         event = SlackUrlVerificationEvent(**event_wrapper.event)
         # Respond to initial URL verification:
         # See: https://api.slack.com/events-api#events_api_request_urls
@@ -44,14 +74,76 @@ def handle_event_callback(event: dict):
 
     if event_type == "app_mention":
         event = SlackAppMentionEvent(**event)
-        logging.debug("Received app mention: {e}".format(e=event))
-        user = slack_users_info(event.user)
-        send_message(event.channel, "Hello @{name}".format(
+        logger.debug("Received app mention: {e}".format(e=event))
+        user = slack.users_info(event.user)
+        slack.send_message(event.channel, "Hello @{name}".format(
             name=user['profile']['display_name']))
         return {"message": "ok"}
     else:
-        logging.error("Cannot handle event: {e}".format(e=event))
+        logger.error("Cannot handle event: {e}".format(e=event))
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST,
                             detail="Unhandled event type: {et}".format(
                                 et=event_wrapper.type))
 
+@app.middleware("http")
+async def verify_slack_signature(request: Request, call_next):
+    """
+    Slack Webhook client verification
+
+    https://api.slack.com/docs/verifying-requests-from-slack
+    """
+    version = "v0"
+
+    # Collect the body content:
+    body = await request.body()
+    body = body.decode("utf-8")
+
+    # Calling request.body "consumed" the body.
+    # Recreate the original request so that it remains intact:
+    request = Request()
+
+
+    # Get signature in request header:
+    try:
+        signature = request.headers['X-Slack-Signature']
+    except KeyError:
+        logger.error("No X-Slack-Signature header found in request")
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
+
+    # Get timestamp in request header:
+    try:
+        timestamp = request.headers['X-Slack-Request-Timestamp']
+    except KeyError:
+        logger.error("No X-Slack-Request-Timestamp header found in request")
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
+
+    ## Prevent replay attacks by ignoring any requests outside the time window:
+    time_window = 5 * 60
+    time_lag = abs(time.time() - float(timestamp))
+    if time_lag > time_window:
+        logger.error(
+            "Event outside of time window by {time_lag} seconds".format(
+                time_lag=time_lag))
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
+
+    # Verify signature:
+    try:
+        verified = slack.verify_signature(
+            signature,
+            timestamp,
+            body,
+            config['SLACK_SIGNING_SECRET'],
+            version)
+        if not verified:
+            raise slack.InvalidSlackSignatureException("slack.verify_signature returned False")
+    except Exception as e:
+        logger.error(type(e))
+        logger.error(e)
+        logger.error(traceback.format_exc())
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
+
+
+    logger.info("Slack signature verified")
+    # Process the request and await response:
+    response = await call_next(request)
+    return response
--- app/slack.py
+++ app/slack.py
@@ -2,29 +2,51 @@ import os
 import requests
 import json
 import logging
+import hmac
+import hashlib
+
+logger = logging.getLogger("slack")
+logger.setLevel(logging.DEBUG)
 
 # logging.getLogger("urllib3").setLevel(logging.DEBUG)
+
+class InvalidSlackSignatureException(Exception):
+    pass
+
+def api(method, data):
+    token = os.environ["SLACK_TOKEN"]
+    url = "https://slack.com/api/{method}".format(method=method)
+    headers = {"Authorization": "Bearer {t}".format(t=token)}
+    logger.debug("headers: {h}".format(h=headers))
+    res = requests.post(url, headers=headers, data=data)
+    if res.status_code != 200:
+        logger.error("Slack API returned status: {status}".format(status=res.status))
+        logger.error(res.content)
+    return json.loads(res.content)
+
+def verify_signature(signature, timestamp, body, signing_key, version="v0"):
+    sig_basestring = "{version}:{timestamp}:{body}".format(
+        version=version, timestamp=timestamp, body=body)
+    logger.debug("signature: {s}".format(s=signature))
+    logger.debug("sig_basestring: {s}".format(s=sig_basestring))
+    digest = hmac.new(signing_key.encode("utf-8"),
+             msg=sig_basestring.encode("utf-8"),
+             digestmod=hashlib.sha256).hexdigest()
+    computed_signature = "{version}={digest}".format(version=version, digest=digest)
+    logger.debug("computed_signature: {s}".format(s=computed_signature))
+
+    return signature == computed_signature
 
 def send_message(channel, message):
     try:
         webhook_url = os.environ["SLACK_WEBHOOK_{channel}".format(channel=channel)]
     except KeyError:
-        logging.error("No known webhook for channel: {channel} - "
-                      "must provide SLACK_CHANNEL_{channel} environment variable.".format(channel=channel))
+        logger.error("No known webhook for channel: {channel} - "
+                      "must provide SLACK_WEBHOOK_{channel} environment variable.".format(channel=channel))
         raise
     requests.post(webhook_url, json={"text": message})
 
-def slack_api(method, data):
-    token = os.environ["SLACK_TOKEN"]
-    url = "https://slack.com/api/{method}".format(method=method)
-    headers = {"Authorization": "Bearer {t}".format(t=token)}
-    logging.debug("headers: {h}".format(h=headers))
-    res = requests.post(url, headers=headers, data=data)
-    if res.status_code != 200:
-        logging.error("Slack API returned status: {status}".format(status=res.status))
-        logging.error(res.content)
-    return json.loads(res.content)
 
-def slack_users_info(user):
-    logging.debug("asking for info for user: {}".format(user))
-    return slack_api("users.info", {"user": user, "include_locale": "false"})['user']
+def users_info(user):
+    logger.debug("asking for info for user: {}".format(user))
+    return api("users.info", {"user": user, "include_locale": "false"})['user']
